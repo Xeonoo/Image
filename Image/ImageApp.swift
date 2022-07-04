a4756fc7625fd588eb43edff208e4278c3c87e7e
Maksymilian <ts3joker@gmail.com>
2022-07-04 13:45:50 +0200

    Initial Commit

---
+++ Image/ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageApp.swift
+//  Image
+//
+//  Created by Maksymilian Patoń on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
