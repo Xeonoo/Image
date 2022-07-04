3860ee41478b59e6db5b94e19e2d8b8b5de0badf
Maksymilian <ts3joker@gmail.com>
2022-07-04 14:18:08 +0200

    1

--- Image/ContentView.swift
+++ Image/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+        
+        ZStack {
+            Image("toronto").resizable().aspectRatio(contentMode: .fit).cornerRadius(10)
+            
+            VStack {
+                Text("Toront CN")
+                    .font(.largeTitle)
+                    .padding([.top, .leading, .trailing])
+                Text("Tower")
+                    .font(.caption)
+                    .padding([.leading, .bottom, .trailing])
+            }
+            .background(Color.black)
+            .foregroundColor(Color.white)
+            .opacity(0.8)
+        }.padding()
+        
+        ZStack {
+            Image("london").resizable().aspectRatio(contentMode: .fit).cornerRadius(10)
+                
+            VStack {
+                Text("Big Ben")
+                    .font(.largeTitle)
+                    .padding([.top, .leading, .trailing])
+                Text("Tower")
+                    .font(.caption)
+                    .padding([.leading, .bottom, .trailing])
+            }
+            .background(Color.black)
+            .foregroundColor(Color.white)
+            .opacity(0.8)
+        }.padding()
+            
+            }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+ 
